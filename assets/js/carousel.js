bc7ccc27df14e9466310bcf1cffc4347d4310532
Andy Ernst <AndyCErnst@gmail.com>
2015-07-28 20:08:54 -0700

    start carousel

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,18 +1,18 @@
-var $carousel = $('#carousel-container');
+var $carousel = $('#carousel');
 var dataIndex = 0;
 
 //  populate starting items
-for(var i = 0; i < 5; i++) {
-  addItem();
-}
+// for(var i = 0; i < 5; i++) {
+//   addItem();
+// }
 
 function addItem(){
   // loop items when reaching end
   if(dataIndex >= carouselData.length) {
     dataIndex = 0;
   }
-  $carousel.append(carouselData[dataIndex++]);
-};
+  $carousel.append('<span class="carousel-item">'+carouselData[dataIndex++]+'</span>');
+}
 
 // add item to the end every two seconds
 setInterval(addItem, 2000);
